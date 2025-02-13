16f581db6d0b35a38976bd0222051e5ddf407e24
tetracalibers <tomix05may@gmail.com>
2025-02-13 10:13:47 +0900

    add "breakpoint-conditional" demo

---
+++ breakpoint-conditional/app.js
@@ -0,0 +1,9 @@
+function milliseconds(x) {
+  return x * 1000
+}
+
+console.log(milliseconds(1))
+console.log(milliseconds(2))
+console.log(milliseconds(3))
+console.log(milliseconds('4s'))
+console.log(milliseconds(5))
