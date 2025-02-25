8c310d36093ff20da05af39a4b93231eb0fbb4e3
tomixy <92695929+tetracalibers@users.noreply.github.com>
2025-02-25 15:15:18 +0900

    add "breakpoint-event-multiple-elements" demo

---
+++ breakpoint-event-multiple-elements/app.js
@@ -0,0 +1,15 @@
+const btnA = document.getElementById('btn-A')
+const btnB = document.getElementById('btn-B')
+const btnC = document.getElementById('btn-C')
+
+btnA.addEventListener('click', function () {
+  console.log('A clicked!')
+})
+
+btnB.addEventListener('click', function () {
+  console.log('B clicked!')
+})
+
+btnC.addEventListener('click', function () {
+  console.log('C clicked!')
+})
